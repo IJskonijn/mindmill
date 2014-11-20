a4b7dd4b5296cfda41d99712edb97cb2656cc4db
soufianeabba <s1072390@student.windesheim.nl>
2014-11-20 18:51:25 +0100

    Added latest GMap.net dll
    Added MapEvent

---
+++ pristine-gatherings/Model/MapEvent.cs
@@ -0,0 +1,53 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PristineGatherings
+{
+    public class MapEvent
+    {
+        private Marker marker;     
+        private string eventName;
+        private string eventInformation;
+        private bool isDeleted;
+
+        public bool IsDeleted
+        {
+            get { return isDeleted; }
+            set { isDeleted = value; }
+        }
+
+        internal Marker Marker
+        {
+            get { return marker; }
+            set { marker = value; }
+        }
+
+        public MapEvent(string locationKeywords, Form1 gmap)
+        {
+            this.isDeleted = false;
+
+            GeoCoderStatusCode status = GeoCoderStatusCode.Unknow;
+            PointLatLng? pos = GMapProviders.GoogleMap.GetPoint(locationKeywords, out status);
+
+            if(pos != null && status == GeoCoderStatusCode.G_GEO_SUCCESS)
+            {
+                marker = new Marker(pos.Value, PristineGatherings.Properties.Resources.map_marker);
+                gmap.MarkersOverlay.Markers.Add(marker);
+                gmap.MarkersOverlay.Control.Invalidate(); 
+            }
+            else
+            {
+                MessageBox.Show("Location not found.");
+            }
+
+            
+        }
+
+    }
+}
