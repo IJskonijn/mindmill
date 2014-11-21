e9b3eb13d2dabb1bb11f187dfc828f5152ed9a42
Berwout <bdvr@live.nl>
2014-11-21 15:23:11 +0100

    asd

--- pristine-gatherings/Form1.Designer.cs
+++ pristine-gatherings/Form1.Designer.cs
@@ -39,7 +39,9 @@
             // 
             this.gmap.Bearing = 0F;
             this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
             this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gmap.LevelsKeepInMemmory = 5;
             this.gmap.Location = new System.Drawing.Point(0, 0);
             this.gmap.Margin = new System.Windows.Forms.Padding(2);
@@ -52,6 +54,8 @@
             this.gmap.PolygonsEnabled = true;
             this.gmap.RetryLoadTile = 0;
             this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gmap.ShowTileGridLines = false;
             this.gmap.Size = new System.Drawing.Size(629, 409);
             this.gmap.TabIndex = 0;
